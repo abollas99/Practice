f9a93bd028cecb58159bf24af3d8f14f3c3715e7
Anthony Bollas <anthonybollas@Anthonys-MacBook-Pro.local>
2022-09-14 00:09:10 -0400

    09/14/22: Complete the States Guessing Game.

--- StatesGame/main.py
+++ StatesGame/main.py
@@ -4,4 +4,29 @@ import csv
 
 screen = turtle.Screen()
 screen.title("US Map")
+image = "blank_states_img.gif"
+screen.addshape(image)
+screen.setup(height=491, width=725)
+turtle.shape(image)
 
+data = pandas.read_csv(f"50_states.csv")
+all_states = data.state.to_list()
+
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title="Guess the state", prompt="Whats another states name?").title()
+    print(answer_state)
+    if answer_state == "Exit":
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
+
+
+screen.exitonclick()
